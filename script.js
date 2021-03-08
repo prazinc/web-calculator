ee4a0d40d158e2abd6b87979a483bfc2cbb350db
Robin Brooks <robin@kairosent.com>
2021-03-08 18:51:20 -0500

    completed work

---
+++ script.js
@@ -0,0 +1,9 @@
+function calc() {
+    var a =  parseInt(document.getElementById("1stnumber").value); 
+    var b =  parseInt(document.getElementById("2ndnumber").value); 
+    var result = a + b;
+    document.getElementById("3rdnumber").value = result
+    
+ 
+ }  
+   
